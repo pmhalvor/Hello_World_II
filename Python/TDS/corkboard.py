f5fc9c9d9738ca080108fd062084042205434dda
perhalvorsen <pmchalvorsen@gmail.com>
2019-07-08 15:47:32 +0200

    Corkboard Version 1.0
    Finally a working program! The program does exactly what I want it to (for now). I'll use it over the next few weeks to see if I can make things even more user friendly.

--- Python/TDS/corkboard.py
+++ Python/TDS/corkboard.py
@@ -2,62 +2,61 @@ import tkinter as tk
 import os
 
 """
-Working on:
--write to files
--layout nicely
--switch from idea->task->working->complete
--graph showing how much of each task there is
--
-
 Problems:
 - Prioritize OO-design, or as few lines as possible
 
-"""
 
-###################### GLOBAL VARIABLES ##########################
-number_of_tasks = 0
-moving = False
-
-######################### FUNCITONS ##############################
-"""
 NOTES:
 Button funcitons cannot have any parameters in order but be callable w/ button
 
-
-Functions needed for working system:
-v create files
-v create lists  #done in scene setup
-v update_list(fname):
-	will update list from respective files w/ lists
-- add_to_board(entry, boardname):
-	will add task to specified board
-- delete:
-	to delete from both list and file
-
-- move(frm, to):
-	will move selected item from one board to next
-- save_data():
-	will save all the lists to their respective file names
-	might not need this if i update file every time new task is added to list
-	but in good OO-nomenclature save_data should remain its own indvd. task
+Should be much easier to make as OO prog since each quad is doing the same thing
+I just want to have a working prog before trying to build in java
 """
 
+######################### FUNCITONS ##############################
+
 def create_file(str):
+	"""
+	Creates file \'str.txt\' if not found in directory
+
+	Parameters:
+
+		str (string)
+			String name of file
+	"""
 	if not os.path.exists(str+'.txt'): #broke up str and .txt to give list title
 		f = open(str+'.txt', 'w')
 		f.write(str+' List:\n') #list title
 		f.close()
 
 def update_list(fname, lstbx):
+	"""
+	Reads previously saved lists and loads data to respective listboxes
+
+	Parameters:
+		fname (string)
+			name of file containing list
+
+		lstbx (Listbox)
+			list which will be updated
+
+	"""
 	f = open(fname, 'r')
 	f.readline()
 	for line in f:
-		lstbx.insert(tk.END, line)
+		lstbx.insert(tk.END, line.strip('\n'))
 	f.close()
 
-def add_Idea():
+def add_Idea(event=None):
+	"""
+	Takes current entry box and inputs to Idea listbox and file
+
+	"""
 	input = entry.get().strip()
 	if input:
+		if is_repeat(input, Idea): #makes sure there are no repeats
+			print("Found repeat")
+			return
 		f = open('Idea.txt', 'a')
 		f.write(input+'\n')
 		f.close()
@@ -65,8 +64,15 @@ def add_Idea():
 		entry.delete(0, tk.END)
 
 def add_ToDo():
+	"""
+	Takes current entry box and inputs to ToDo listbox and file
+
+	"""
 	input = entry.get().strip()
 	if input:
+		if is_repeat(input, ToDo): #makes sure there are no repeats
+			print("Found repeat")
+			return
 		f = open('ToDo.txt', 'a')
 		f.write(input+'\n')
 		f.close()
@@ -74,8 +80,15 @@ def add_ToDo():
 		entry.delete(0, tk.END)
 
 def add_Work():
+	"""
+	Takes current entry box and inputs to Work listbox and file
+
+	"""
 	input = entry.get().strip()
 	if input:
+		if is_repeat(input, Work): #makes sure there are no repeats
+			print("Found repeat")
+			return
 		f = open('Work.txt', 'a')
 		f.write(input+'\n')
 		f.close()
@@ -83,8 +96,15 @@ def add_Work():
 		entry.delete(0, tk.END)
 
 def add_Done():
+	"""
+	Takes current entry box and inputs to Done listbox and file
+
+	"""
 	input = entry.get().strip()
 	if input:
+		if is_repeat(input, Done): #makes sure there are no repeats
+			print("Found repeat")
+			return
 		f = open('Done.txt', 'a')
 		f.write(input+'\n')
 		f.close()
@@ -92,13 +112,92 @@ def add_Done():
 		entry.delete(0, tk.END)
 
 def move_to_ToDo(event):
-	entry.insert(0, Idea.get(Idea.curselection())) #to avoid having to write new
-	add_ToDo() #calls same method as above
-	Idea.delete(Idea.curselection()) #deletes from previous list.
+	"""
+	Double click action that moves task from Idea to ToDo
 
-def delete(lstbox):
-	#I dont have nay good way of deleting abstractly. Need to think a more here
-	return 0
+	"""
+	entry.insert(0, Idea.get(Idea.curselection())) #to avoid having to write new
+	str = Idea.get(Idea.curselection())
+	delete_from_file('Idea.txt', str)
+	Idea.delete(Idea.curselection()) #deletes from previous list.
+	add_ToDo() #calls same method as above
+
+def move_to_Work(event):
+	"""
+	Double click action that moves task form ToDo to Work
+
+	"""
+	entry.insert(0, ToDo.get(ToDo.curselection())) #to avoid having to write new
+	str = ToDo.get(ToDo.curselection())
+	delete_from_file('ToDo.txt', str)
+	ToDo.delete(ToDo.curselection()) #deletes from previous list.
+	add_Work() #calls same method as above
+
+def move_to_Done(event):
+	"""
+	Double click action that moves task from Work to Done
+
+	"""
+	entry.insert(0, Work.get(Work.curselection())) #to avoid having to write new
+	str = Work.get(Work.curselection())
+	delete_from_file('Work.txt', str)
+	Work.delete(Work.curselection()) #deletes from previous list.
+	add_Done() #calls same method as above
+
+def delete_from_done(event):
+	"""
+	Double click action that just deletes from Done list and file
+
+	"""
+	str = Done.get(Done.curselection())
+	delete_from_file('Done.txt', str)
+	Done.delete(Done.curselection())
+
+def delete_from_file(fname, remove_str):
+	"""
+	Helper function which removes one line containing remove_str. After the
+	requested line to be removed is skipped, remove_str is changed so that the
+	file will always match the Listbox.
+
+	Parameters:
+		fname (string)
+			name of file to be changed
+
+		remove_str (string)
+			data which is supposed to be removed from file
+
+	"""
+	print('\nRemoving '+remove_str+' from '+fname+':\n')
+	with open(fname, 'r') as f: #new formet. see how it works
+		lines = f.readlines()
+	with open(fname, 'w') as f:
+		for line in lines:
+			if line.strip() != remove_str.strip():
+				print('rewriting: ' +line.strip('\n'))
+				f.write(line)
+			else:
+				print('REMOVED: '+ line.strip('\n'))
+				remove_str = '------------------' #cant make blank or null
+
+
+def is_repeat(str, lstbx):
+	"""
+	Helper function that checks is current input is already in list
+
+	Parameters:
+		str (string)
+			current input
+
+		lstbx (Listbox)
+			the list which needs to be checked fro repeats
+
+	"""
+	for i in range(lstbx.size()):
+		if str.strip() == lstbx.get(i).strip():
+			entry.delete(0, tk.END)
+			return True
+	return False
+
 
 ##################### SCENE SET ###########################
 master = tk.Tk()
@@ -109,35 +208,11 @@ create_file('ToDo')
 create_file('Work')
 create_file('Done')
 
+
 #Label
 l = tk.Label(master, text='Task:')
 l.grid(row=1,column=0)
 
-#Inbox
-entry = tk.Entry(master)
-entry.grid(row=1, column=1, columnspan=4)
-
-# Create listboxes and insert to scene
-Idea = tk.Listbox(master)
-Idea.grid(row=3, column=0)
-Idea.bind('<Double-Button-1>', move_to_ToDo)
-
-ToDo = tk.Listbox(master)
-ToDo.grid(row=3, column=1)
-# ToDo.bind('<Double-Button-1>', move_to_Work)
-
-Work = tk.Listbox(master)
-Work.grid(row=3, column=2)
-# Work.bind('<Double-Button-1>', move_to_Done)
-
-Done = tk.Listbox(master)
-Done.grid(row=3, column=3)
-
-# Update lists if files already exsisted
-update_list('Idea.txt', Idea)
-update_list('ToDo.txt', ToDo)
-update_list('Work.txt', Work)
-update_list('Done.txt', Done)
 
 #Buttons
 Q = tk.Button(master, text='Quit', command=master.quit)
@@ -155,4 +230,37 @@ W.grid(row=2, column=2, sticky=tk.W) #adds to stage
 D = tk.Button(master, text='Done', command=add_Done)
 D.grid(row=2, column=3, sticky=tk.W) #adds to stage
 
+
+#Inbox
+entry = tk.Entry(master)
+entry.grid(row=1, column=1, columnspan=4)
+master.bind('<Return>', add_Idea)
+
+
+# Create listboxes and insert to scene
+Idea = tk.Listbox(master)
+Idea.grid(row=3, column=0)
+Idea.bind('<Double-Button-1>', move_to_ToDo)
+
+ToDo = tk.Listbox(master)
+ToDo.grid(row=3, column=1)
+ToDo.bind('<Double-Button-1>', move_to_Work)
+
+Work = tk.Listbox(master)
+Work.grid(row=3, column=2)
+Work.bind('<Double-Button-1>', move_to_Done)
+
+Done = tk.Listbox(master)
+Done.grid(row=3, column=3)
+Done.bind('<Double-Button-1>', delete_from_done)
+
+
+# Update lists if files already exsisted
+update_list('Idea.txt', Idea)
+update_list('ToDo.txt', ToDo)
+update_list('Work.txt', Work)
+update_list('Done.txt', Done)
+
+
+# Start everything
 master.mainloop()
