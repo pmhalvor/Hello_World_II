759fcd8c8c328ff1e1b7a18446c3e2d61b4512fc
pmhalvor <pmhalvor@users.noreply.github.com>
2019-09-30 22:47:15 +0200

    Added README to Java. Should add same to each task. Will come with next

---
+++ Java/README.md
@@ -0,0 +1,17 @@
+This page consists of a few of the projects I was assigned throughout my Introduction to Java course in the spring of 2019.
+
+### Decryption with Threads
+This is a encryption/decryption system using threads to execute as fast as possible.
+
+### Labyrinth
+This is a GUI of a maze-game. Click on a box and the program will show you the fastest route out.
+
+### Linked Lists
+My own personal Linked List class, along with a Sorted Linked List and a Stack.
+All three are children of the abstract List class.
+
+### Cluster
+This was our first assignment where we were to create pur own version of Clusters
+
+### Hospital System
+System containing doctors, patient, prescriptions, medicines and other typical traits one would expect to find in a hospital.
--- Java/SimpleClusters/MainProgram.java
+++ Java/SimpleClusters/MainProgram.java
@@ -2,27 +2,6 @@ import java.io.FileNotFoundException;
 
 public class MainProgram{
     public static void main(String[] args){
-        // ComputerCluster abel = new ComputerCluster(12);
-        // int c = 0;
-        // for (int a=0; a<650; a++){
-        //     Node n = new Node(64, 1);
-        //     abel.addNode(n);
-        //     if (c<16){
-        //         // System.out.println("c="+c);
-        //         c++;
-        //         Node m = new Node(1024, 2);
-        //         abel.addNode(m);
-        //     }
-        // }
-        // int tP = abel.totalProcessors();
-        // System.out.println("Total processors " + tP);
-        // int mem32 = abel.nodesWithEnoughMemory(32);
-        // int mem64 = abel.nodesWithEnoughMemory(64);
-        // int mem128 = abel.nodesWithEnoughMemory(128);
-        // System.out.println("At least 32: " + mem32);
-        // System.out.println("At least 64: " + mem64);
-        // System.out.println("At least 128: " + mem128);
-        // System.out.println("Nmber of racks: " + abel.numOfRacks());
 
         ComputerCluster readAbel = new ComputerCluster("regneklynge.txt"); //1 works (kinda), 2 works, 3 works, 4 works
         int totProcessors = readAbel.totalProcessors();
@@ -31,7 +10,7 @@ public class MainProgram{
         int mem128 = readAbel.nodesWithEnoughMemory(128);
         System.out.println("At least 32 GB: " + mem32);
         System.out.println("At least 64 GB: " + mem64);
-        System.out.println("At least 128 GB: " + mem128);  
+        System.out.println("At least 128 GB: " + mem128);
         System.out.println("Total racks: " + readAbel.numOfRacks());
     }
 }
